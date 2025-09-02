4f5597b1796d38d1c06f4574a3f46d83131a90a8
takay99 <167079875+takay99@users.noreply.github.com>
2025-09-02 18:31:03 +0900

    imu can be read

--- Core/Src/main_thread.cpp
+++ Core/Src/main_thread.cpp
@@ -4,14 +4,9 @@
 #include <stm32rcos/core.hpp>
 #include <stm32rcos/hal.hpp>
 #include <stm32rcos/peripheral.hpp>
-#include <stm32rcos_drivers/bno055.hpp>
-#include <stm32rcos_drivers/c6x0.hpp>
 
-#include "bno055.h"
-#include "lowpass.hpp"
-#include "output_to_logger.hpp"
-
-extern "C" void bno055_assignI2C(I2C_HandleTypeDef *hi2c_device);
+#include "bno055.hpp"
+#include "g4_i2c.hpp"
 
 extern UART_HandleTypeDef huart2;
 extern UART_HandleTypeDef huart1;
@@ -54,52 +49,76 @@ extern TIM_HandleTypeDef htim2;
 // }
 ////////
 
-#include "bno055.h"
-
-#define I2C_HandleTypeDef int;
 // I2C_HandleTypeDef *_bno055_i2c_port;
 
 extern "C" void main_thread(void *) {
   using namespace stm32rcos::core;
   using namespace stm32rcos::peripheral;
-  using namespace stm32rcos_drivers;
 
   Uart<&huart2> uart2; // デバッグ出力用
   enable_stdout(uart2);
 
-  Can<&hfdcan1> can;
-  C6x0Manager c610_manager(can);
-  C6x0 c610_1(c610_manager, C6x0Type::C610, C6x0Id::ID_1); // front motor
-  C6x0 c610_2(c610_manager, C6x0Type::C610, C6x0Id::ID_2); // rear motor
-  can.start();
+  I2C i2c_28(&hi2c1, 0x29); // BNO055のI2Cアドレスは0x28
+  I2C i2c_29(&hi2c1, 0x28); // BNO055のI2Cアドレスは0x28
+  Bno055 bno055_28(i2c_28);
+  Bno055 bno055_29(i2c_29);
 
-  LowPassFilter lowpass_filter_1;
-  LowPassFilter lowpass_filter_2;
-  LowPassFilter lowpass_filter_vel_1;
-  LowPassFilter lowpass_filter_vel_2;
+  while (!bno055_28.start(1000)) {
+    printf("Failed to initialize BNO055. Retrying...\n");
+    osDelay(100);
+  }
 
-  LowPassFilter lowpass_filter_imu;
-
-  HAL_TIM_IC_Start_IT(&htim3, TIM_CHANNEL_1);
-  HAL_TIM_IC_Start_IT(&htim3, TIM_CHANNEL_2);
-
-  HAL_TIM_IC_Start_IT(&htim2, TIM_CHANNEL_1);
-  HAL_TIM_IC_Start_IT(&htim2, TIM_CHANNEL_2);
-
-  // lowpass_state_t lowpass_state = {0};
-  bno055_assignI2C(&hi2c1);
-  bno055_setup();
-  bno055_setOperationModeNDOF();
+  while (!bno055_29.start(1000)) {
+    printf("Failed to initialize BNO055. Retrying...\n");
+    osDelay(100);
+  }
 
   while (true) {
-    uint32_t start = osKernelGetTickCount();
 
-    auto euler = bno055_getVectorEuler();
+    Quaternion quat_28, quat_29;
+    AngularVelocity ang_vel_28, ang_vel_29;
+    Acceleration acc_28, acc_29;
 
-    printf("%.2f %.2f %.2f %.2f\r\n", euler.w, euler.x, euler.y, euler.z);
+    // if (bno055_28.get_quaternion(quat_28)) {
+    //   printf("Quaternion_28: w=%.3f, x=%.3f, y=%.3f, z=%.3f\r\n", quat_28.w,
+    //          quat_28.x, quat_28.y, quat_28.z);
+    // } else {
+    //   printf("Failed to get quaternion data from BNO055.\n");
+    // }
+    // if (bno055_29.get_quaternion(quat_29)) {
+    //   printf("Quaternion_29: w=%.3f, x=%.3f, y=%.3f, z=%.3f\r\n", quat_29.w,
+    //          quat_29.x, quat_29.y, quat_29.z);
+    // } else {
+    //   printf("Failed to get quaternion data from BNO055.\n");
+    // }
 
-    // lowpass_filter(&lowpass_state, 0.0f, start / 1000.0f);
+    // if (bno055_28.get_angular_velocity(ang_vel_28)) {
+    //   printf("Quaternion_28: x=%.3f, y=%.3f, z=%.3f\r\n", ang_vel_28.x,
+    //          ang_vel_28.y, ang_vel_28.z);
+    // } else {
+    //   printf("Failed to get quaternion data from BNO055.\n");
+    // }
 
-    osDelayUntil(start + 10);
+    // if (bno055_29.get_angular_velocity(ang_vel_29)) {
+    //   printf("Quaternion_29: x=%.3f, y=%.3f, z=%.3f\r\n", ang_vel_29.x,
+    //          ang_vel_29.y, ang_vel_29.z);
+    // } else {
+    //   printf("Failed to get quaternion data from BNO055.\n");
+    // }
+
+    if (bno055_28.get_acceleration(acc_28)) {
+      printf("Quaternion_28: x=%.3f, y=%.3f, z=%.3f\r\n", acc_28.x, acc_28.y,
+             acc_28.z);
+    } else {
+      printf("Failed to get quaternion data from BNO055.\n");
+    }
+
+    if (bno055_29.get_acceleration(acc_29)) {
+      printf("Quaternion_29: x=%.3f, y=%.3f, z=%.3f\r\n", acc_29.x, acc_29.y,
+             acc_29.z);
+    } else {
+      printf("Failed to get quaternion data from BNO055.\n");
+    }
+    osDelay(10);
   }
 }
